2334e07a2584b1f40b05f18d3b228caec7c6ac2f
FredricLaestander <fredriclaestander@gmail.com>
2025-04-17 18:25:36 +0200

    install lucide

---
+++ src/icons.ts
@@ -0,0 +1,24 @@
+import {
+  createIcons,
+  EllipsisVertical,
+  House,
+  ImageOff,
+  Plus,
+  Search,
+  Settings,
+  Trash2,
+  X,
+} from "lucide";
+
+createIcons({
+  icons: {
+    EllipsisVertical,
+    House,
+    ImageOff,
+    Plus,
+    Search,
+    Settings,
+    Trash2,
+    X,
+  },
+});
