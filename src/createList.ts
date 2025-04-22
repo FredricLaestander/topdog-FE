c830ffd8a8d6872398247bf10717db7ded7385dd
FredricLaestander <fredriclaestander@gmail.com>
2025-04-22 20:06:08 +0200

    create list page

---
+++ src/createList.ts
@@ -0,0 +1,58 @@
+import "./style.css";
+import { header } from "./header";
+
+function main() {
+  const accessToken = localStorage.getItem("access-token");
+
+  if (!accessToken) {
+    window.location.pathname = "/log-in";
+    return;
+  }
+
+  header();
+
+  const url = `${import.meta.env.VITE_BACKEND_URL}/lists`;
+
+  const name = document.querySelector<HTMLInputElement>("#name")!;
+  const description = document.querySelector<HTMLInputElement>("#description")!;
+  const errorSpan = document.querySelector<HTMLSpanElement>("#error")!;
+
+  const createListForm = document.getElementById("createList")!;
+
+  createListForm.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    try {
+      const values = {
+        name: name.value,
+        description: description.value,
+      };
+
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + accessToken,
+        },
+        body: JSON.stringify(values),
+      });
+
+      const data = await response.json();
+
+      if (!response.ok) {
+        throw new Error(
+          data.errorMessage ||
+            "Dear dog, you have an unexpected error. Please try refreshing the site. Love -The team"
+        );
+      }
+
+      window.location.pathname = `/list?id=${data.listId}`;
+    } catch (error) {
+      if (error instanceof Error) {
+        errorSpan.innerText = error.message;
+      }
+    }
+  });
+}
+
+main();
