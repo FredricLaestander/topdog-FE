5912f9c5aaa0a3e0854ee98d13729063ba4a56ac
FredricLaestander <fredriclaestander@gmail.com>
2025-04-24 18:00:15 +0200

    add vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,21 @@
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        list: resolve(__dirname, "list.html"),
+        createList: resolve(__dirname, "create-list.html"),
+        logIn: resolve(__dirname, "log-in.html"),
+        signUp: resolve(__dirname, "sign-up.html"),
+      },
+    },
+  },
 });
